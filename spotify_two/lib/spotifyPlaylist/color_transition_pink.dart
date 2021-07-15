9ea9c2091a1ebe9fc9b2bf3bf55ba14502098b76
Haticenurcoskunn <70744158+Haticenurcoskunn@users.noreply.github.com>
2021-07-15 14:26:43 +0300

    Color updated

--- spotify_two/lib/spotifyPlaylist/color_transition_pink.dart
+++ spotify_two/lib/spotifyPlaylist/color_transition_pink.dart
@@ -4,7 +4,7 @@ import 'package:spotify/my_general_widget.dart/color_transition.dart';
 
 BoxDecoration buildColorTransition() {
   return colortransition(
-    startColor: Color(0xFFCA8ADD),
+    startColor: Color(0xFFA56CB6),
     endColor: Colors.black.withOpacity(0.88),
     beginAlignment: Alignment.topCenter,
     endAlignment: Alignment.bottomCenter,
